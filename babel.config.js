ec394612708c01a19c14305d919ed83323b85eb6
gcaaa31928 <gcaaa31928@gmail.com>
2020-03-18 01:01:04 +0800

    fixed: added babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,29 @@
+module.exports = function (api) {
+	if (api.env('test')) {
+		return {
+			"presets": [
+				[
+					"@babel/preset-env",
+					{
+						targets: {
+							node: 'current'
+						}
+					}
+				]
+			]
+		}
+	}
+	return {
+		"presets": [
+			[
+				"@babel/preset-env",
+				{
+					modules: false
+				}
+			]
+		],
+		"plugins": [
+			"@babel/plugin-transform-runtime"
+		]
+	};
+}
